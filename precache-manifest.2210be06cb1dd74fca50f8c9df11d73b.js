2ef84f0c37110396422176c576e49600ca85d28d
Sirion <thesirion@gmail.com>
2020-05-11 20:47:28 -0300

    Updates

---
+++ precache-manifest.2210be06cb1dd74fca50f8c9df11d73b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d248e8945628d980942f3ace43a0a4",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "12b3a7b8ebedb9d187ea",
+    "url": "/face-recognition-brain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "d88eeaabe89bb53dbf60",
+    "url": "/face-recognition-brain/static/css/main.df9a80f2.chunk.css"
+  },
+  {
+    "revision": "12b3a7b8ebedb9d187ea",
+    "url": "/face-recognition-brain/static/js/2.b72cfb87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-brain/static/js/2.b72cfb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88eeaabe89bb53dbf60",
+    "url": "/face-recognition-brain/static/js/main.043cde64.chunk.js"
+  },
+  {
+    "revision": "92d6de68486dff0da434",
+    "url": "/face-recognition-brain/static/js/runtime-main.a894f7e1.js"
+  }
+]);
